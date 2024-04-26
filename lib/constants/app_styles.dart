c3bb65f9a38ead3eec5c3391d7016495316230c7
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-04-26 20:29:10 +0800

    added constants and diff stuff

---
+++ lib/constants/app_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kLightGrey = Color(0xFFF3F4F8);
+const Color kPink = Color(0xFFFA0078);
+const Color kMediumGrey = Color(0xFF808080);
+const Color kDarkBlue = Color(0xFF111828);
+const Color kGrey = Color(0xFF666666);
+const Color kDarkGrey = Color(0xFF8D8D8D);
+const Color kDarkerGrey = Color(0xFF454545);
+const Color kAsh = Color(0xFF9B9B9B);
+const Color kLightBlack = Color(0xFF272727);
+const Color kGreen = Color(0xFF36C07E);
+const Color kLightGreyer = Color(0xFFE8E8E8);
+const Color kContraGrey = Color(0xFF9B9B9B);
+
+final SoraBold = GoogleFonts.sora(
+  color: kDarkBlue,
+  fontWeight: FontWeight.w700,
+);
+final SoraSemiBold = GoogleFonts.sora(
+  color: kDarkBlue,
+  fontWeight: FontWeight.w600,
+);
+final SoraRegular = GoogleFonts.sora(
+  color: kDarkBlue,
+  fontWeight: FontWeight.w400,
+);
+final SoraLight = GoogleFonts.sora(
+  color: kDarkBlue,
+  fontWeight: FontWeight.w300,
+);
+final SoraExtraLight = GoogleFonts.sora(
+  color: kDarkBlue,
+  fontWeight: FontWeight.w200,
+);
+final SoraThin = GoogleFonts.sora(
+  color: kDarkBlue,
+  fontWeight: FontWeight.w100,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+class Main extends StatefulWidget {
+  const Main({super.key});
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  @override
+  State<Main> createState() => _MainState();
+}
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
