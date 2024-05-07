20c2a2b74a1fac97d1c9b5d4ebb0dd1386ebecd4
lec <dev.lecth@gmail.com>
2024-05-07 18:36:03 +0800

    added color constants

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xff121212);
+  static const Color spotifyGreen = Color(0xff1ED760);
+  static const Color darkAccent = Color(0xff030303);
+  static const Color white = Color(0xffffffff);
+  static const Color darkGrey = Color(0xff777777);
+  static const Color lightGrey = Color(0xffC4C4C4);
+}
