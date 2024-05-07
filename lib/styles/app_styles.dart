beed362a0f027c7b12dac744be396397e3f0c3d5
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-05-07 18:07:31 +0800

    updated app_Styles

--- lib/styles/app_styles.dart
+++ lib/styles/app_styles.dart
@@ -4,32 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const Color kWhite = Color(0xFFFFFFFF);
-const Color kPink = Color(0xFFE9A6A6);
-const Color kDarkGrey = Color(0xFF14181c);
-const Color kGrey = Color(0xff485464);
-const Color kLightBlue = Color(0xffddeeff);
+const Color kDarkGrey = Color(0xFF282828);
+const Color kBlack = Color(0xFF121212);
+const Color kGrey = Color(0xff777777);
+const Color kGreen = Color(0xFF1ED760);
 
-final OpenSansBold = GoogleFonts.openSans(
+final SenMedium = GoogleFonts.sen(
   color: kWhite,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w500,
 );
-final OpenSansRegular = GoogleFonts.openSans(
-  color: kWhite,
-  fontWeight: FontWeight.w400,
-);
-final OpenSansSemiBold = GoogleFonts.openSans(
+
+final SenSemiBold = GoogleFonts.sen(
   color: kWhite,
   fontWeight: FontWeight.w600,
 );
-final RobotoSemiBold = GoogleFonts.roboto(
-  color: kWhite,
-  fontWeight: FontWeight.w600,
-);
-final RobotoRegular = GoogleFonts.roboto(
-  color: kWhite,
-  fontWeight: FontWeight.w400,
-);
-final RobotoBold = GoogleFonts.roboto(
+
+final SenBold = GoogleFonts.sen(
   color: kWhite,
   fontWeight: FontWeight.bold,
 );
