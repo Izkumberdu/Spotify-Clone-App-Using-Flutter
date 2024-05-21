438275ca2d651d3ab2e71309a0e27112b84dbd80
lec <dev.lecth@gmail.com>
2024-05-21 18:52:28 +0800

    Auto stash before rebase of "HEAD"
    bruh

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lettersquared/screens/choose_artist.dart';
 import 'package:lettersquared/screens/homepage.dart';
 import 'package:lettersquared/screens/library.dart';
 import 'package:lettersquared/screens/onboarding.dart';
 import 'package:lettersquared/screens/searchMenu.dart';
+import 'package:lettersquared/screens/trackview.dart';
 import 'package:lettersquared/screens/signup1.dart';
 import 'package:lettersquared/screens/start.dart';
 
@@ -19,14 +21,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application. test
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ChooseArtist(),
-      // routes: {
-      //   '/onboarding': (context) => const Onboarding(),
-      //   '/searchMenu': (context) => const SearchMenu(),
-      //   '/homepage': (context) => const Homepage(),
-      //   '/library': (context) => const Library(),
-      // },
+        '/onboarding': (context) => const Onboarding(),
     );
   }
 }
