576ae19c1f245ad2d46cafc68eb97616dd21ffd7
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-05-27 03:13:00 +0800

    cleaning stuff

--- lib/screens/signup_screens/signup_password.dart
+++ lib/screens/signup_screens/signup_password.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:lettersquared/audio/song_handler.dart';
 import 'package:lettersquared/screens/signup_screens/signup_email.dart';
